2e25f5fe108e48e72d58b6843c5f1b6396462264
misinayu <yama_masa22@yahoo.co.jp>
2016-10-19 18:20:47 +0900

    MultiActivityのプロジェクトを追加

---
+++ MultiActivity/app/src/main/java/com/example/masanori/multiactivity/FirstActivity.java
@@ -0,0 +1,25 @@
+package com.example.masanori.multiactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button btnNext = (Button) findViewById(R.id.button1);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
