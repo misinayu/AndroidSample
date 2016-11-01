a52a30df6f6b7f5e486ce442140687f87fc73bb5
misinayu <yama_masa22@yahoo.co.jp>
2016-11-01 22:24:23 +0900

    AlarmNotiプロジェクトを完成
    AlarmNotiプロジェクトとはNotificationを利用して決めた時間に通知を表示するアプリ

---
+++ AlarmNoti/app/src/main/java/com/example/masanori_acer/alarmnoti/AlarmReceiver.java
@@ -0,0 +1,35 @@
+package com.example.masanori_acer.alarmnoti;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by MASANORI on 2016/11/01.
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        int notificationId = intent.getIntExtra("notificationId", 0);
+        NotificationManager myNotification =
+                (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent bootIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, bootIntent, 0);
+        Notification.Builder builder = new Notification.Builder(context); // ノーティフィケーションの作成
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("時間ですよ")
+                .setContentText(intent.getCharSequenceExtra("todo"))
+                .setWhen(System.currentTimeMillis())
+                .setPriority(Notification.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setContentIntent(contentIntent);
+
+        myNotification.notify(notificationId, builder.build()); // ノーティフィケーションの発行
+    }
+}
---
+++ AlarmNoti/app/src/main/java/com/example/masanori_acer/alarmnoti/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.masanori_acer.alarmnoti;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
+    private InputMethodManager mInputMethodManager;
+    private RelativeLayout mLayout;
+    private int notificationId = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText edText = (EditText)findViewById(R.id.editText);
+        mLayout = (RelativeLayout)findViewById(R.id.activity_main);
+        // キーボード表示を制御するためのオブジェクト
+        mInputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        Button btnSet = (Button)findViewById(R.id.set);
+        btnSet.setOnClickListener(this);
+        Button btnCancel = (Button)findViewById(R.id.cancel);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // ソフトキーボードを非表示にする
+        mInputMethodManager.hideSoftInputFromWindow(mLayout.getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+        // フォーカスを背景に
+        mLayout.requestFocus();
+
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText edText = (EditText)findViewById(R.id.editText);
+        // AlarmReceiverを呼び出すインテント
+        // マニフェストファイルにreceiverが必要
+        Intent bootIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        // 追加データとして、Notificationの識別子を渡す
+        bootIntent.putExtra("notificationId", notificationId);
+        // 追加データとして、やることを渡す
+        bootIntent.putExtra("todo", edText.getText());
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0,
+                bootIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarm = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        TimePicker tPicker = (TimePicker)findViewById(R.id.timePicker);
+
+        switch (v.getId()){
+            case R.id.set:
+                int hour = tPicker.getCurrentHour();
+                int minute = tPicker.getCurrentMinute();
+
+                Calendar startTime = Calendar.getInstance();
+                startTime.set(Calendar.HOUR_OF_DAY, hour);
+                startTime.set(Calendar.MINUTE, minute);
+                startTime.set(Calendar.SECOND, 0);
+                long alarmStartTime = startTime.getTimeInMillis();
+
+                alarm.set(
+                        AlarmManager.RTC_WAKEUP,
+                        alarmStartTime,
+                        alarmIntent
+                );
+                Toast.makeText(MainActivity.this, "通知をセットしました！", Toast.LENGTH_SHORT).show();
+                notificationId++;
+
+                break;
+            case R.id.cancel:
+                alarm.cancel(alarmIntent);
+                Toast.makeText(MainActivity.this, "通知をキャンセルしました！", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
