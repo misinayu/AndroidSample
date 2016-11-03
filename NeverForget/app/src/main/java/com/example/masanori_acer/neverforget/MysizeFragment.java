eedc37c1439dccc2143a1c885420614300ef9d7f
misinayu <yama_masa22@yahoo.co.jp>
2016-11-03 22:12:00 +0900

    NeverForgetプロジェクトのMysizeFragmentを完成させた

--- NeverForget/app/src/main/java/com/example/masanori_acer/neverforget/MysizeFragment.java
+++ NeverForget/app/src/main/java/com/example/masanori_acer/neverforget/MysizeFragment.java
@@ -1,12 +1,14 @@
 package com.example.masanori_acer.neverforget;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -58,6 +60,76 @@ public class MysizeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        SharedPreferences pref = this.getActivity().getSharedPreferences("mysize", Context.MODE_PRIVATE);
+        int neck = pref.getInt("neck", 0);
+        int sleeve = pref.getInt("sleeve", 0);
+        int waist = pref.getInt("waist", 0);
+        int insideLeg = pref.getInt("insideLeg", 0);
+        EditText edText1 = (EditText) getView().findViewById(R.id.editText1);
+        if (neck != 0){
+            edText1.setText(Integer.toString(neck));
+        }
+        EditText edText2 = (EditText) getView().findViewById(R.id.editText2);
+        if (neck != 0){
+            edText2.setText(Integer.toString(sleeve));
+        }
+        EditText edText3 = (EditText) getView().findViewById(R.id.editText3);
+        if (neck != 0){
+            edText3.setText(Integer.toString(waist));
+        }
+        EditText edText4 = (EditText) getView().findViewById(R.id.editText4);
+        if (neck != 0){
+            edText4.setText(Integer.toString(insideLeg));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        EditText edText1 = (EditText)getView().findViewById(R.id.editText1);
+        EditText edText2 = (EditText)getView().findViewById(R.id.editText2);
+        EditText edText3 = (EditText)getView().findViewById(R.id.editText3);
+        EditText edText4 = (EditText)getView().findViewById(R.id.editText4);
+        int neck;
+        try{
+            neck = Integer.parseInt(edText1.getText().toString());
+        }catch (NumberFormatException e){
+            neck = 0;
+        }
+        int sleeve;
+        try{
+            sleeve = Integer.parseInt(edText1.getText().toString());
+        }catch (NumberFormatException e){
+            sleeve = 0;
+        }
+        int waist;
+        try{
+            waist = Integer.parseInt(edText1.getText().toString());
+        }catch (NumberFormatException e){
+            waist = 0;
+        }
+        int insideLeg;
+        try{
+            insideLeg = Integer.parseInt(edText1.getText().toString());
+        }catch (NumberFormatException e){
+            insideLeg = 0;
+        }
+
+        SharedPreferences pref = this.getActivity().getSharedPreferences("mysize", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putInt("neck", neck);
+        editor.putInt("sleeve", sleeve);
+        editor.putInt("waist", waist);
+        editor.putInt("insideLeg", insideLeg);
+        // editor.commit();
+        editor.apply(); // commitの非同期版
     }
 
     @Override
