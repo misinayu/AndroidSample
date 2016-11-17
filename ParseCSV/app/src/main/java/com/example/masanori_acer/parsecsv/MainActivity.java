ea7b4b900e213b93109dd58e08c85771d5f0ea57
misinayu <yama_masa22@yahoo.co.jp>
2016-11-17 21:48:46 +0900

    csvを読み込んで表示するだけのプロジェクトの完成

--- ParseCSV/app/src/main/java/com/example/masanori_acer/parsecsv/MainActivity.java
+++ ParseCSV/app/src/main/java/com/example/masanori_acer/parsecsv/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.masanori_acer.parsecsv;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv01 = (TextView)findViewById(R.id.txt01);
+        AssetManager assetManager = getResources().getAssets();
+        try{
+            InputStream is = assetManager.open("output.csv");
+            InputStreamReader inputStreamReader = new InputStreamReader(is);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = "";
+            StringBuilder strBuild = new StringBuilder();
+            while ((line =bufferedReader.readLine()) != null){
+                StringTokenizer st = new StringTokenizer(line, ",");
+                while (st.hasMoreTokens()){
+                    strBuild.append(st.nextToken());
+                    strBuild.append(",");
+                }
+                strBuild.append("\n");
+            }
+            tv01.setText(strBuild.toString());
+            bufferedReader.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
