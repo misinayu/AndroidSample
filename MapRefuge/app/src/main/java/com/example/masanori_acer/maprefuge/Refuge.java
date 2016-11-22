fc5b93b3a7f73294737cbba72ccde22820209052
misinayu <yama_masa22@yahoo.co.jp>
2016-11-22 16:48:04 +0900

    MapRefugeプロジェクトの完成
    野々市の避難所をGoogleMap上で表示してくれるアプリ
    地図をタップしたら近くの避難所をマーキングしてくれるよ

---
+++ MapRefuge/app/src/main/java/com/example/masanori_acer/maprefuge/Refuge.java
@@ -0,0 +1,55 @@
+package com.example.masanori_acer.maprefuge;
+
+/**
+ * Created by MASANORI on 2016/11/17.
+ */
+
+public class Refuge {
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
+    private boolean near;
+    private int distance;
+
+    public Refuge(String name, String address, double latitude, double longitude){
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.near = false;
+        this.distance = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getLat() {
+        return latitude;
+    }
+
+    public double getLng() {
+        return longitude;
+    }
+
+    public boolean isNear(){
+        return near;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setNear(boolean near) {
+        this.near = near;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = (int) distance;
+    }
+}
